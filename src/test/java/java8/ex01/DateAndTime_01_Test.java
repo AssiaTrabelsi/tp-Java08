da97e9c2ab1a762e57613ca6a77c68f49c3a05f7
ETY0003 <ETY0003@DESKTOP-L9R2IUA>
2017-11-17 17:28:37 +0100

    Date pas fini

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -18,13 +18,13 @@ public class DateAndTime_01_Test {
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(117, 2, 24, 1,2,3);
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
-
+  date.setDate(25);
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
@@ -35,12 +35,24 @@ public class DateAndTime_01_Test {
         Calendar calendar = Calendar.getInstance();
 
         // TODO modifier l'objet calendar pour que le test soit passant
+        
+        calendar.set(calendar.YEAR, 2017);
+        calendar.set(calendar.MONTH, 2);
+        calendar.set(calendar.DAY_OF_MONTH , 24);
+        calendar.set(calendar.HOUR_OF_DAY,1);
+        calendar.set(calendar.MINUTE, 2);
+        calendar.set(calendar.SECOND, 3);
 
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
-
+        
+        
+        
+  calendar.add(Calendar.DAY_OF_MONTH,1);
+  
+  
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
